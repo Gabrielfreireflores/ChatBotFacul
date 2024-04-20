36c39f52dadd66a7e0dba7952148a772b2ef7054
sestari <sestari99@gmail.com>
2024-04-20 11:54:33 -0300

    feat/V.1.1.2

--- intents.js
+++ intents.js
@@ -1,40 +1,4 @@
-const agendamento = [
-  'confirmar',
-  'marcar',
-  'aceito',
-  'aceitar',
-  'confirmar',
-  'sim'
-];
-
-const consultas = [
-  'nova',
-  'marca',
-  'marcar uma nova',
-  'nova consulta',
-  'agendar consulta',
-  'agendar',
-  'consulta',
-  'marcar consulta',
-  'consultar',
-  'marcar',
-  'sim',
-  'marcar uma nova consulta'
-];
-
-const verConsultas = [
-  'ver',
-  'consultas',
-  'minhas consultas',
-  'consulta',
-  'consult',
-  'agendadas',
-  'ver minhas consultas',
-  'quero ver',
-  'mostrar',
-  'ver consulta'
-];
-
+//array inicial
 const greetings = [
   'oi', 
   'ola', 
@@ -47,9 +11,74 @@ const greetings = [
   'Oi, boa noite',
   'Oi!',
   'Oi.',
-  'Oi, tudo bem?'
+  'Oi, tudo bem?',
+  'marcar',
 ];
 
+//desejea cadastrar => sim / registrar
+const registrar = [
+  'register',
+  'novo usuario',
+  'registro',
+  'quero registrar',
+  'registrar',
+  'sim',
+  'reg',
+];
+
+//deseja cadastrar => nao / cancelar registro
+const naoRegistrar = [
+  'nao register',
+  'não novo usuario',
+  'n',
+  'não',
+  'nem',
+  'nãooo',
+  'cancelar',
+  'encerrar',
+  'não registrar',
+  'nao',
+];
+
+
+//marcar consulta
+const consultas = [
+  'nova',
+  'marca',
+  'marcar uma nova',
+  'nova consulta',
+  'agendar consulta',
+  'agendar',
+  'consulta',
+  'marcar consulta',
+  'consultar',
+  'marcar',
+  'sim',
+  'marcar uma nova consulta',
+  's',
+  'quero',
+  'quer',
+  'claro',
+];
+
+
+//ver consulta
+const verConsultas = [
+  'ver',
+  'consultas',
+  'minhas consultas',
+  'consulta',
+  'consult',
+  'agendadas',
+  'ver minhas consultas',
+  'quero ver',
+  'mostrar',
+  'ver consulta',
+  'marcada',
+];
+
+
+//confirmar a consulta => sim
 const confirmar = [
   'confirmar',
   'sim',
@@ -62,11 +91,27 @@ const confirmar = [
   'marcar uma nova consulta'
 ]
 
+//confirmar a consulta => nao
+const naoMarcar = [
+  'nao',
+  'não',
+  'n',
+  'não',
+  'nem',
+  'nãooo',
+  'cancelar',
+  'encerrar',
+  'não registrar',
+  'nao',
+]
+
 // Exporte as listas para uso em outros arquivos usando CommonJS
 module.exports = {
-  agendamento,
+  registrar,
+  naoRegistrar,
   consultas,
   greetings,
   verConsultas,
-  confirmar
+  confirmar, 
+  naoMarcar
 };
